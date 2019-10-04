89f5b945971b76b1a7c311d05d51fdbf31fad264
roberta costa <robertacs42@gmail.com>
2019-10-04 17:13:34 -0300

    com spell check

--- Bow_dou.py
+++ Bow_dou.py
@@ -10,6 +10,7 @@ def main():
     filelist = []  # lista de arquivos .json da pasta no caminho acima
     output_list = []
     publicacao_dict = {}
+    output_dict = {}
     for f in path_data.iterdir():
         filelist.append(f)
 
@@ -21,7 +22,7 @@ def main():
             data = str(datetime.date(int(file.name[:4]), int(file.name[4:6]), int(file.name[6:8])))  # ano, mês, dia
             i = 0
         for publicacao in json_data:
-            i = i+1
+            i += 1
             print(i)
             title_string = publicacao['title']
             body_string = publicacao['body']
@@ -30,22 +31,27 @@ def main():
             # Normalização do texto de uma publicação
             tokens = Text_normalization.text_tokenize(publicacao_text.lower())
             filtered_tokens = Text_normalization.remove_specialchar(tokens)
-            corrected_tokens = Text_normalization.spell_check(filtered_tokens)
+            corrected_tokens, corrected_words = Text_normalization.spell_check(filtered_tokens)
             filtered_text = Text_normalization.remove_stopwords(corrected_tokens)
             stemmed_tokens = Text_normalization.stemming(filtered_text)
             word_counts = Text_normalization.count_word(stemmed_tokens)
-            publicacao_dict['data'] = data
+            publicacao_dict['date'] = data
             publicacao_dict['bow'] = word_counts
-            publicacao_dict['texto'] = publicacao_text
+            publicacao_dict['text'] = publicacao_text
             # publicacao_dict['risco'] =
             output_list.append(publicacao_dict.copy())  # sem o copy a última publicação sobrescreve todas as anteriores
+            output_dict.update(corrected_words)
 
         with open(output_file, 'w') as outfile:
             json.dump(output_list, outfile)
             output_list.clear()
             print(datetime.datetime.now().time())
 
-        # database.post_publicacoes(output_file)
+    # database.post_publicacoes(output_file)
+
+    with open('output_dict', 'w') as outfile:
+        json.dump(output_dict, outfile)
+
 
         # como pesquisar as publicacoes com risco no mongodb
 
--- Text_normalization.py
+++ Text_normalization.py
@@ -7,6 +7,11 @@ from spellchecker import SpellChecker
 
 # Separa todas os elementos do texto
 def text_tokenize(text):
+    """
+
+    :param text:
+    :return:
+    """
     tokens = nltk.word_tokenize(text)
     tokens = [token.strip() for token in tokens]
     return tokens
@@ -14,6 +19,11 @@ def text_tokenize(text):
 
 # Retira os caracteres especiais
 def remove_specialchar(tokens):
+    """
+
+    :param tokens:
+    :return:
+    """
     pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
     filtered_tokens = filter(None, [pattern.sub('', token) for token in tokens])
     return filtered_tokens
@@ -21,6 +31,11 @@ def remove_specialchar(tokens):
 
 # Retira stop words
 def remove_stopwords(tokens):
+    """
+
+    :param tokens:
+    :return:
+    """
     stopwords = nltk.corpus.stopwords.words('portuguese')
     filtered_tokens = [token for token in tokens if token not in stopwords]
     filtered_text = ' '.join(filtered_tokens)
@@ -29,6 +44,11 @@ def remove_stopwords(tokens):
 
 # Stemming
 def stemming(filtered_text):
+    """
+
+    :param filtered_text:
+    :return:
+    """
     stemmer = nltk.stem.RSLPStemmer()
     stemmed_text = stemmer.stem(filtered_text)
     stemmed_tokens = text_tokenize(stemmed_text)
@@ -37,22 +57,38 @@ def stemming(filtered_text):
 
 # Conta as palavras e suas frequencias
 def count_word(tokens):
+    """
+
+    :param tokens:
+    :return:
+    """
     word_counts = collections.Counter(tokens)
     return word_counts
 
 
 def spell_check(tokens):
+    """
+
+    :param tokens:
+    :return:
+    """
+    corrected_dict = {}
+    output_corrected_list = {}
     corrected_tokens = list(tokens)
     spell = SpellChecker(distance=2, language='pt')  # recomendada para palavras grandes
+    spell.word_frequency.load_text_file('./romans_up_to_3999.txt')
     misspelled = spell.unknown(corrected_tokens)
     for word in misspelled:
         if word != spell.correction(word):
             corrected_tokens.append(spell.correction(word))
         else:
             misspelled = [token for token in misspelled if token != word]
+        corrected_dict[word] = spell.correction(word)
+        output_corrected_list.update(corrected_dict)
     corrected_tokens = [token for token in corrected_tokens if token not in misspelled]
-    return corrected_tokens
+    return corrected_tokens, output_corrected_list
 
 
-cor = remove_stopwords(spell_check(remove_specialchar(text_tokenize('agricuura'))))
-print(cor)
+# cor, out = spell_check(remove_specialchar(text_tokenize('agricuura qulquer agricuura qlquer XXII')))
+# print(cor)
+# print(out)
